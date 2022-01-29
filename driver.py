8860e79fd15eab6d30183a121e4b1ad0be1fb29d
Akash Karanam <Karanam.akash1002@rice.edu>
2022-01-29 02:10:25 -0600

    integrated multiple data streams

--- driver.py
+++ driver.py
@@ -50,8 +50,7 @@ def format_data(url):
     
     df = pd.DataFrame(useful_array, columns = ['year', 'month', 'day', 'hour', 'minutes', 'wind direction', 'wind speed', 'wind gust'])
 
-            
-
+    df['Source'] = url[len(url) - 8: len(url) - 4]
 
     return df
 
@@ -80,6 +79,49 @@ def plot(df):
         plt.clf()
 
 
+def multiplot(df):
+    plt.ion()
+    i = 0
+    while i < df.shape[0]:
+        tranch = []
+        tranch.append(i)
+        tranch_date = (df.loc[i, 'month'], df.loc[i, 'day'], df.loc[i, 'year'], df.loc[i, 'hour'], df.loc[i, 'minutes'])
+        flag = False
+        while i+1 < df.shape[0] and (df.loc[i+1, 'month'], df.loc[i+1, 'day'], df.loc[i+1, 'year'], df.loc[i+1, 'hour'], df.loc[i+1, 'minutes']) == tranch_date:
+            flag = True
+            tranch.append(i+1)
+            i = i + 1
+        if flag == False:
+            i = i + 1
+
+        input_array = []
+        colors = []
+        color_mapping = {'KIKT': 'r', 'KBQX':'b', 'KMIS':'g'}
+        for index in tranch:
+            anglestr = df.loc[index, 'wind direction']
+            speedstr = df.loc[index, 'wind speed']
+
+            angle = float(anglestr)
+            speed = float(speedstr)
+
+            x = speed * math.cos(math.radians(90 - angle))
+            y = speed * math.sin(math.radians(90 - angle))
+            input_array.append([x,y])
+            colors.append(color_mapping[df.loc[index, 'Source']])
+        
+        origin_input = []
+        origin_input.append([0] * len(input_array))
+        origin_input.append([0] * len(input_array))
+
+        vector = np.array(input_array)
+        origin = np.array(origin_input)
+        
+        plt.quiver(*origin, vector[:,0], vector[:,1], color=colors, scale=60)
+        plt.draw()
+        date = tranch_date[0] + "/" + tranch_date[1] + "/" + tranch_date[2] + " " + tranch_date[3] + ":" + tranch_date[4]
+        plt.text(-0.04, 0.04, date, fontsize = 14)
+        plt.pause(.001)
+        plt.clf()
 
 
 
@@ -89,9 +131,24 @@ def plot(df):
 
 def main():
     urlKIKT = "https://www.ndbc.noaa.gov/data/realtime2/KIKT.txt"
+    urlKAPT = "https://www.ndbc.noaa.gov/data/realtime2/KBQX.txt"
+    urlKMIS = "https://www.ndbc.noaa.gov/data/realtime2/KMIS.txt"
     kikt_data = format_data(urlKIKT)
+    kapt_data = format_data(urlKAPT)
+    kmis_data = format_data(urlKMIS)
+
+    result = pd.concat([kikt_data,kapt_data,kmis_data], ignore_index=True)
+    result.sort_values(by=['year', 'month', 'day', 'hour', 'minutes'], inplace=True, ignore_index=True)
+    result.reindex()
+    print(result.shape)
     # print_formatted_data(kikt_data)
-    plot(kikt_data)
+    # print_formatted_data(kapt_data)
+    # print_formatted_data(kmis_data)
+    # print_formatted_data(result)
+
+    multiplot(result)
+    # print_formatted_data(kikt_data)
+    # plot(kikt_data)
     
 
 
